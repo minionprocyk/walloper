f5c21e8e0096dfd83b8c77fcf205fa3d24c413d8
Procyk <eveonlinealpha001@gmail.com>
2018-10-28 15:17:39 -0400

    Added Last_Frontier py program

--- operationv/src/Last_Frontier.py
+++ operationv/src/Last_Frontier.py
@@ -12,11 +12,13 @@ if docked == 'n':
 elif docked == "y":
     print('Currently Docked')
 elif docked == '?':
-    #evaluate if docked or not using ingame memory reading method / logic
+    print('Not sure if docked')
+#evaluate if docked or not using ingame memory reading method / logic
 else:
     print('Error @ "Am I docked?"')
 
 # If I am docked then undock
+evaluate_docked == '?'
 while docked == 'y':
     print('execite undock feature')
     print('execute evaluate if docked / undocked feature')
@@ -25,15 +27,34 @@ while docked == 'y':
     elif evaluate_docked =='y':
         docked == 'y'
     else:
-        [print('Error @ "If I am docked, then undock"')
-
-
-if docked == 'y':
-    print('execute undock feature')
-    print('execute ')
+        print('Error @ "If I am docked, then undock"')
 
 # select destination
+destination_selected ='?'
+evaluate_destination_selected ='?'
+while destination_selected != 'y':
+    print('execute select destination feature')
+    evaluate_destination_selected ='execute evaluate select destination feature'
+    print(evaluate_destination_selected)
+    if evaluate_destination_selected == 'y':
+        destination_selected = 'y'
+    elif evaluate_destination_selected == 'n':
+        destination_selected = 'n'
+    else:
+        print('Error @ "select destination"')
+
 # Enable auto pilot
+auto_pilot = '?'
+while auto_pilot != 'on':
+    print('execite enable_auto_pilot')
+    evaluate_auto_pilot='execute evaluate_enable_auto_pilot'
+    if evaluate_auto_pilot == 'y':
+        auto_pilot = 'on'
+    elif evaluate_auto_pilot == 'n':
+        auto_pilot ='off'
+    else:
+        print('Error @ Enable autopilot')
+
 # Have I reached my destiniation? or Am I Docked?
 # - Micro Navigation -
 def micro_navigation():
