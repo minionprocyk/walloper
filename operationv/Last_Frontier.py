f7afe463d1274e8155c5a3e0c784553043d40454
Procyk <eveonlinealpha001@gmail.com>
2018-10-28 13:28:29 -0400

    Added Last_Frontier py program

--- operationv/Last_Frontier.py
+++ operationv/Last_Frontier.py
@@ -3,9 +3,35 @@
 # -- Navigation --
 # - Macro Navigation -
 def macro_navigation():
-    print("Text")
+    print("macro_navigation")
+
 # Am I docked?
+docked = '?'
+if docked == 'n':
+    print('Currently Undocked')
+elif docked == "y":
+    print('Currently Docked')
+elif docked == '?':
+    #evaluate if docked or not using ingame memory reading method / logic
+else:
+    print('Error @ "Am I docked?"')
+
 # If I am docked then undock
+while docked == 'y':
+    print('execite undock feature')
+    print('execute evaluate if docked / undocked feature')
+    if evaluate_docked == 'n':
+        docked == 'n'
+    elif evaluate_docked =='y':
+        docked == 'y'
+    else:
+        [print('Error @ "If I am docked, then undock"')
+
+
+if docked == 'y':
+    print('execute undock feature')
+    print('execute ')
+
 # select destination
 # Enable auto pilot
 # Have I reached my destiniation? or Am I Docked?
