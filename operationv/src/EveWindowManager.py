b1a6b42cc8007ef621d0b1fea8d64e17be7bd23c
Procyk <eveonlinealpha001@gmail.com>
2018-10-28 18:10:41 -0400

    Added Last_Frontier py program

--- operationv/src/EveWindowManager.py
+++ operationv/src/EveWindowManager.py
@@ -9,6 +9,12 @@ class EveWindowManager:
     def click_something(self):
         pyautogui.click(100,100,1)
 
+    def undock(self):
+        undock_location = pyautogui.locateOnScreen('Undock.png')
+        undock_location_x,undock_location_y = pyautogui.center(undock_location)
+        pyautogui.click(undock_location_x,undock_location_y)
+
+
 
 if __name__ == '__main__':
     window_manager = EveWindowManager()
